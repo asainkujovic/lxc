949f7b3ce0ea5186f4771d77a513664cceb9d668
Asain Kujovic <asainnp@gmail.com>
2025-01-26 03:56:31 +0100

    LXC attach exit on SIGCHLD corrections and ignore-param
    Signed-off-by: Asain Kujovic <asainnp@gmail.com>

--- src/lxc/attach.c
+++ src/lxc/attach.c
@@ -1430,6 +1430,15 @@ static inline void lxc_attach_terminal_close_log(struct lxc_terminal *terminal)
 	close_prot_errno_disarm(terminal->log_fd);
 }
 
+void lxc_attach_sig_handler(int signum)
+{
+	if (signum == SIGCHLD) {
+		/* ignore + raise() = do nothing but break [epool/io_ring]_wait */
+		signal(SIGTERM, SIG_IGN);
+		raise(SIGTERM);
+	}
+}
+
 int lxc_attach(struct lxc_container *container, lxc_attach_exec_t exec_function,
 	       void *exec_payload, lxc_attach_options_t *options,
 	       pid_t *attached_process)
@@ -1741,6 +1750,10 @@ int lxc_attach(struct lxc_container *container, lxc_attach_exec_t exec_function,
 		goto close_mainloop;
 
 	TRACE("Transient process %d exited", pid);
+
+	/* After trans-proc exit, add SIGCHLD handler if option flag is set */
+	if (options->attach_flags & LXC_ATTACH_SIGCHLD_EXIT)
+		signal(SIGCHLD, lxc_attach_sig_handler);
 
 	/* We will always have to reap the attached process now. */
 	to_cleanup_pid = attached_pid;
